bc0ac035c4f1e6934d4a0e14dab5bc152f64ee57
Pedro Lucas França <73343509+fpedrolucas95@users.noreply.github.com>
2022-02-04 23:48:15 -0300

    Create code.java

---
+++ src/code/code.java
@@ -0,0 +1,36 @@
+package code;
+
+public class code {
+	
+	public double somar(double n1, double n2) {
+		double resultado = n1+n2;
+		return resultado;
+	}
+
+	public double subtrair(double n1, double n2) {
+		double resultado = n1-n2;
+		return resultado;
+	}
+	
+	public double multiplicar(double n1, double n2) {
+		double resultado = n1*n2;
+		return resultado;
+	}
+	
+	public double dividir(double n1, double n2) {
+		double resultado = n1/n2;
+		return resultado;
+	}
+	
+	public double raizQ(double n1) {
+		double resultado = Math.sqrt(n1);
+		return resultado;
+	}
+	
+	public double reciproco(double n1) {
+		double resultado = 1/n1;
+		return resultado;
+	}
+	
+
+}
