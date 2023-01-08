c20295223d70cfe3bf15f24230018df7da2bb086
Pedro Lucas França <73343509+fpedrolucas95@users.noreply.github.com>
2023-01-08 03:37:54 -0300

    otimização do código

--- src/code/code.java
+++ src/code/code.java
@@ -1,36 +1,28 @@
 package code;
 
 public class code {
-	
-	public double somar(double n1, double n2) {
-		double resultado = n1+n2;
-		return resultado;
-	}
+public static double somar(double n1, double n2) {
+return n1 + n2;
+}
 
-	public double subtrair(double n1, double n2) {
-		double resultado = n1-n2;
-		return resultado;
-	}
-	
-	public double multiplicar(double n1, double n2) {
-		double resultado = n1*n2;
-		return resultado;
-	}
-	
-	public double dividir(double n1, double n2) {
-		double resultado = n1/n2;
-		return resultado;
-	}
-	
-	public double raizQ(double n1) {
-		double resultado = Math.sqrt(n1);
-		return resultado;
-	}
-	
-	public double reciproco(double n1) {
-		double resultado = 1/n1;
-		return resultado;
-	}
-	
+Copy code
+public static double subtrair(double n1, double n2) {
+    return n1 - n2;
+}
 
+public static double multiplicar(double n1, double n2) {
+    return n1 * n2;
+}
+
+public static double dividir(double n1, double n2) {
+    return n1 / n2;
+}
+
+public static double raizQ(double n1) {
+    return Math.sqrt(n1);
+}
+
+public static double reciproco(double n1) {
+    return 1 / n1;
+}
 }
